ce6e7c677c43f9aaa9d6854a86d57307794f295a
Ayasalem127 <aya.salem@viacesi.fr>
2025-06-12 09:41:08 +0200

    ajout de authenticate au gateway

--- user-service/src/models/UserProfile.js
+++ user-service/src/models/UserProfile.js
@@ -4,6 +4,11 @@ const userProfileSchema = new mongoose.Schema({
   userId: { type: String, required: true, unique: true },
   displayName: { type: String },
   bio: { type: String },
+   role: { 
+    type: String, 
+    enum: ['user', 'moderator', 'admin'], 
+    default: 'user' 
+  },
   avatarUrl: { type: String },
   followers: [{ type: String, ref: 'UserProfile' }],
   following: [{ type: String, ref: 'UserProfile' }]

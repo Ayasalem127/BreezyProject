bec649adcd7c86ca956315aa161bb7f22dd46d3f
Hatice Yildiz <hatice.yildiz@viacesi.fr>
2025-06-13 11:37:38 +0200

    modif logo et titre

--- frontend/kittens-app/src/app/userCreation/page.js
+++ frontend/kittens-app/src/app/userCreation/page.js
@@ -4,8 +4,8 @@ import UserCreationForm from "@/components/UserCreationForm";
 export default function Home() {
   return (
     <div className="container mx-auto p-4 text-center">
-      <h1 className="text-4xl font-bold text-gray-800 mb-4 p-10">Bienvenue sur Kitties</h1>
-      <img src="/logo.webp" alt="logo" className="w-full h-48 object-contain mb-2" />
+      <h1 className="text-4xl font-bold text-gray-800 mb-4 p-10">Création de compte</h1>
+      <img src="/kitties transparents.webp" alt="logo" className="w-full h-48 object-contain mb-2" />
       <UserCreationForm />
     </div>
   );

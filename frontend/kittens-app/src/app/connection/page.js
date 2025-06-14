e30ac703899382e1a534be1818cbd0793b3cf7b8
Lisa JUNG <lisa.jung@viacesi.fr>
2025-06-14 16:51:56 +0200

    arrangement et style navbar + first + connexion + usercreation

--- frontend/kittens-app/src/app/connection/page.js
+++ frontend/kittens-app/src/app/connection/page.js
@@ -3,10 +3,18 @@ import ConnectionForm from "@/components/ConnectionForm";
 
 export default function Home() {
   return (
-    <div className="container mx-auto p-4 text-center">
-      <h1 className="text-4xl font-bold text-gray-800 mb-4 p-10">Connexion</h1>
-      <img src="/kitties transparents.webp" alt="logo" className="w-full h-48 object-contain mb-2" />
-      <ConnectionForm />
+    <div className="grid grid-cols-1 sm:grid-cols-2 w-full h-screen-minus-navbar">
+      <div className="flex items-center justify-center">
+        <img src="/logo-removebg.png" alt="logo" className="w-full h-150 object-contain mb-2" />
+      </div>
+      <div className="flex flex-col items-center justify-center text-center space-y-4">
+        <div className="flex flex-col items-center justify-center rounded-2xl shadow-2xl border border-gray-500 w-2/3 h-9/10">
+          <h1 className="text-4xl font-bold text-gray-800 mb-4 p-10">Connexion</h1>
+          <main>
+            <ConnectionForm />
+          </main>
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/kittens-app/src/components/Navbar.js
+++ frontend/kittens-app/src/components/Navbar.js
@@ -4,22 +4,21 @@ export default function Navbar() {
   return (
     <nav
       style={{ backgroundColor: "var(--buttons)" }}
-      className="fixed top-0 left-0 w-full h-16 flex items-center justify-between px-6 shadow-md z-50"
+      className="fixed top-0 left-0 w-full h-12 flex items-center justify-between px-6 shadow-md z-50"
     >
       {/* Zone gauche : Logo + Accueil, Abonnements, Messages */}
       <div className="flex items-center space-x-6">
-        {/* 👉 Ton image logo à gauche */}
         <img
-          src="/logo.webp" // remplace par l’image que tu veux
+          src="/kitties transparents.webp" // remplace par l’image que tu veux
           alt="Logo"
-          className="w-10 h-10 object-contain"
+          className="w-5 h-5 object-contain"
         />
 
         <Link href="/home">
           <img
             src="/home.png"
             alt="Accueil"
-            className="w-8 h-8 hover:scale-110 transition-transform"
+            className="w-5 h-5 hover:scale-110 transition-transform"
           />
         </Link>
 
@@ -27,7 +26,7 @@ export default function Navbar() {
           <img
             src="/follow.png"
             alt="Abonnements"
-            className="w-8 h-8 hover:scale-110 transition-transform"
+            className="w-5 h-5 hover:scale-110 transition-transform"
           />
         </Link>
 
@@ -35,7 +34,7 @@ export default function Navbar() {
           <img
             src="/messaging.png"
             alt="Messages"
-            className="w-8 h-8 hover:scale-110 transition-transform"
+            className="w-5 h-5 hover:scale-110 transition-transform"
           />
         </Link>
       </div>
@@ -46,7 +45,7 @@ export default function Navbar() {
           <img
             src="/notification.png"
             alt="Notifications"
-            className="w-8 h-8 hover:scale-110 transition-transform"
+            className="w-5 h-5 hover:scale-110 transition-transform"
           />
         </Link>
 
@@ -54,7 +53,7 @@ export default function Navbar() {
           <img
             src="/logo.webp"
             alt="Profil"
-            className="w-8 h-8 hover:scale-110 transition-transform"
+            className="w-5 h-5 hover:scale-110 transition-transform rounded-full"
           />
         </Link>
       </div>

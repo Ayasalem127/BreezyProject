4ea5314b65d0a705384a4fc1b08afce4dd4b1c29
Lisa JUNG <lisa.jung@viacesi.fr>
2025-06-21 15:00:13 +0200

    Liaison back-front page "Mes conversation"

--- frontend/kittens-app/src/components/ChatList.js
+++ frontend/kittens-app/src/components/ChatList.js
@@ -1,14 +1,31 @@
 'use client';
 import Link from "next/link";
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 function truncate(text, maxLength) {
   return text.length > maxLength ? text.slice(0, maxLength) + "..." : text;
 }
 
 export default function ChatList() {
-    const chats = [["username", "/logo.webp", "Moi", "J'ai envoyé ce message.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "01/01/2001"], ["username", "/logo.webp", "Username", "J'ai reçu ce message.", "01/01/2001"], ["username", "/logo.webp", "Moi", "J'ai envoyé ce message.", "01/01/2001"], ["username", "/logo.webp", "Username", "J'ai reçu ce message.", "01/01/2001"], ["username", "/logo.webp", "Moi", "J'ai envoyé ce message.", "01/01/2001"], ["username", "/logo.webp", "Username", "J'ai reçu ce message.", "01/01/2001"], ["username", "/logo.webp", "Moi", "J'ai envoyé ce message.", "01/01/2001"], ["username", "/logo.webp", "Username", "J'ai reçu ce message.", "01/01/2001"]];
-    
+    //const chats = [["username", "/logo.webp", "Moi", "J'ai envoyé ce message.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "01/01/2001"], ["username", "/logo.webp", "Username", "J'ai reçu ce message.", "01/01/2001"], ["username", "/logo.webp", "Moi", "J'ai envoyé ce message.", "01/01/2001"], ["username", "/logo.webp", "Username", "J'ai reçu ce message.", "01/01/2001"], ["username", "/logo.webp", "Moi", "J'ai envoyé ce message.", "01/01/2001"], ["username", "/logo.webp", "Username", "J'ai reçu ce message.", "01/01/2001"], ["username", "/logo.webp", "Moi", "J'ai envoyé ce message.", "01/01/2001"], ["username", "/logo.webp", "Username", "J'ai reçu ce message.", "01/01/2001"]];
+    const [chats, setChats] = useState([]);
+
+    const getChats = async (e) => {
+        try {
+            const res = await axios.get('http://localhost:3001/messaging/messaging/chats', { withCredentials: true });
+            console.log(res.data);
+            setChats(res.data);
+            
+        } catch (error) {
+            console.error("Erreur : ", error);
+        }
+    }
+
+    useEffect(() => {
+        getChats();
+    }, []);
+
     return (
         <div>
             <div className="flex flex-col items-center w-full px-4">
@@ -16,13 +33,13 @@ export default function ChatList() {
                     <div key={index} className="w-full sm:w-[calc(50%-0.5rem)] p-2 m-4 box-border flex flex-col justify-between border border-gray-500 rounded-2xl shadow-2xl">
                         <Link href={"/chat"}>
                             <div className="p-2 ml-10 box-border flex flex-row items-center justify-start rounded-lg gap-4">
-                                <img src={chat[1]} alt="photo de profil" className="w-10 h-10 object-contain rounded-full"/>
-                                <span className="font-semibold">{chat[0]}</span>
+                                <img src={chat.otherUser.avatarUrl || "/logo.webp"} alt="photo de profil" className="w-10 h-10 object-contain rounded-full"/>
+                                <span className="font-semibold">{chat.otherUser.displayName}</span>
                             </div>
                             <div className="p-2 mr-10 box-border flex flex-row items-center justify-end rounded-lg gap-4">
-                                <p>{chat[2]}</p>
-                                <p>{truncate(chat[3], 50)}</p>
-                                <p>{chat[4]}</p>
+                                <p>{chat.authorId === chat.otherUser.userId ? chat.otherUser.displayName : "Moi"}</p>
+                                <p>{truncate(chat.content, 50)}</p>
+                                <p>{new Date(chat.createdAt).toLocaleDateString()}</p>
                             </div>
                         </Link>
                     </div>

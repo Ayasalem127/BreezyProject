1453deea6434729cbc9846a843de362e3f11533e
Aysee2 <ayse.deniz@viacesi.fr>
2025-06-24 09:26:20 +0200

    affichage des posts et des comm du user spécifique lorsqu'on va sur le profil du user

--- frontend/kittens-app/src/app/otherProfile/page.js
+++ frontend/kittens-app/src/app/otherProfile/page.js
@@ -2,9 +2,17 @@
 import OtherInformations from "@/components/OtherInformations";
 import OtherMessages from "@/components/OtherMessages";
 import { useSearchParams } from "next/navigation";
+import { useEffect} from "react";
+
+
 export default function Home() {
      const searchParams = useSearchParams();
   const userId = searchParams.get("userId");
+
+   useEffect(() => {
+    console.log("🔍 userId reçu dans la page Home:", userId);
+  }, [userId]);
+
   return (
     <div className="w-full px-2 text-center">
 

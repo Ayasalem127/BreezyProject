110a7f50b22190464addafb34259253998f20f71
Ayasalem127 <aya.salem@viacesi.fr>
2025-06-25 16:02:24 +0200

    I

--- frontend/kittens-app/src/components/UsersSuggestionMobile.js
+++ frontend/kittens-app/src/components/UsersSuggestionMobile.js
@@ -57,8 +57,10 @@ export default function UsersSuggestionMobile() {
                 <div key={index} style={{ boxShadow: "0 12px 32px var(--shadow-color)", borderColor: 'var(--input-border)' }} className="w-full p-2 m-4 box-border flex flex-col justify-between border rounded-2xl">
                         <Link href={"/otherProfile"}>
                             <div className="p-2 box-border flex flex-row items-center justify-center rounded-lg gap-4">
-                                <img src={user[1]} alt="photo de profil" className="w-10 h-10 object-contain rounded-full"/>
-                                <span className="font-semibold">{user[0]}</span>
+                                <img src={user?.avatarUrl
+  ? `http://localhost:3001${user.avatarUrl}`
+  : "/avatarcat.jpg"} alt="photo de profil" className="w-10 h-10 object-contain rounded-full"/>
+                                <span className="font-semibold">{user.displayName}</span>
                             </div>
                         </Link>
                 </div>
